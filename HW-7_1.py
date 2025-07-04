8818578fa83f7d63024d4e62cfa81443d3e46d33
Ivan K <kosi17100@gmail.com>
2025-07-04 20:47:54 +0300

    выполнено 3 задания

--- HW-7_1.py
+++ HW-7_1.py
@@ -1,5 +1,4 @@
 
-
 #1
 # def count_calls(func):
 #     n = 0
@@ -21,22 +20,42 @@
 
 #2
 
-def type_check(tuple):
+# def type_check(*types):
+#     def decorator(func):
+#         def wrapper(*args):
+#             for i in range(len(types)):
+#                 if isinstance(args[i], types[i]):
+#                     return func(*args)
+#                 else:
+#                     return f"TypeError: Неверный тип аргумента {args[i]}. Ожидался {type(types[i])}, получен {type(args[i])}"
+#         return wrapper
+#     return decorator
+#
+#
+# @type_check(int, int)
+# def add(a, b):
+#     return a + b
+#
+# print(add(2, 3))     # 5
+# print(add(2, '3'))   # TypeError: Неверный тип аргумента 'b'. Ожидался <class 'int'>, получен <class 'str'>
+
+#3
+
+def validate_range(min_value, max_value):
     def decorator(func):
         def wrapper(*args):
-            for i in tuple:
-                try:
-                    func(*args)
-                except TypeError:
-                    print(f"Неверный тип аргумента {args[i]}. Ожидался {type(tuple[i])}, получен {type(args[i])}")
-
+            for i in args:
+                if i>=min_value and i <= max_value:
+                    return func(*args)
+                else:
+                    print(f"ValueError: Аргумент 'value' имеет значение {i}, что выходит за пределы [{min_value}, {max_value}]")
         return wrapper
+    return decorator
 
-tuple = (int, int)
 
-@type_check(int, int)
-def add(a, b):
-    return a + b
+@validate_range(min_value=0, max_value=100)
+def set_percentage(value):
+    print(f"Установлено значение: {value}%")
 
-print(add(2, 3))     # 5
-print(add(2, '3'))   # TypeError: Неверный тип аргумента 'b'. Ожидался <class 'int'>, получен <class 'str'>
+set_percentage(50)     # Вывод: Установлено значение: 50%
+set_percentage(150)    # ValueError: Аргумент 'value' имеет значение 150, что выходит за пределы [0, 100]
